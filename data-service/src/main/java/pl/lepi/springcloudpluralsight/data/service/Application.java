a6078aa983ac0de50a5c4b5b7d691e5bb6c18a9c
mllk <2019Zawisza>
2019-07-26 14:31:31 +0200

    Added DataService, dir restructure

---
+++ data-service/src/main/java/pl/lepi/springcloudpluralsight/data/service/Application.java
@@ -0,0 +1,25 @@
+package pl.lepi.springcloudpluralsight.data.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@Value("${service.instance.name")
+	private String instance;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String message() {
+		return "Serwus at your service! {^_^}";
+	}
+
+}
