9b71e147a3907ca56460ad8046d087366a1a568c
mllk <2019Zawisza>
2019-07-28 20:36:36 +0200

    added bootstrap.yml

--- data-service/src/main/java/pl/lepi/springcloudpluralsight/data/service/Application.java
+++ data-service/src/main/java/pl/lepi/springcloudpluralsight/data/service/Application.java
@@ -1,16 +1,18 @@
 package pl.lepi.springcloudpluralsight.data.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class Application {
 
-	@Value("${service.instance.name")
+	@Value("${service.instance.name}")
 	private String instance;
 
 	public static void main(String[] args) {
