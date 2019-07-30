ae60505d5471a39bc3554095db4fe5d96c176752
mllk <2019Zawisza>
2019-07-30 11:58:11 +0200

    package name refactor

--- discovery-service/src/main/java/pl/lepi/springcloudpluralsight/discovery/service/Application.java
+++ discovery-service/src/main/java/pl/lepi/springcloudpluralsight/discovery/service/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudPluralSightApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudPluralSightApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
